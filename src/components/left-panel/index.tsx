4af5a61f30c1544cab787fe9c705d882c516a298
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2024-05-24 17:19:11 +0330

    fix: canvas overlapping the left panel making it non interactive

--- src/components/left-panel/index.tsx
+++ src/components/left-panel/index.tsx
@@ -8,7 +8,7 @@ export default function LeftPanel({ hidden = false }: LeftPanelProps) {
   return (
     <div
       className={cn(
-        'absolute bottom-0 left-0 top-0 flex w-80 items-center justify-center border-r transition-transform',
+        'absolute bottom-0 left-0 top-0 z-50 flex w-80 items-center justify-center border-r transition-transform',
         hidden && '-translate-x-full',
       )}
     >
