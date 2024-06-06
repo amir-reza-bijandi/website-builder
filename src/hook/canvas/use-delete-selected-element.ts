1034ff5eb3997511994bfdb66736e8eae698d023
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2024-06-06 14:13:16 +0330

    fix: prevent deleting elements when panning

--- src/hook/canvas/use-delete-selected-element.ts
+++ src/hook/canvas/use-delete-selected-element.ts
@@ -3,22 +3,28 @@ import { useShallow } from 'zustand/react/shallow';
 import useCanvasStore from '@/store/canvas-store';
 
 export default function useDelete() {
-  const { deleteElement, isFocused, selectedElementIdList } = useCanvasStore(
-    useShallow((store) => ({
-      selectedElementIdList: store.selectedElementIdList,
-      deleteElement: store.deleteElement,
-      isFocused: store.isFocused,
-    })),
-  );
+  const { deleteElement, isFocused, selectedElementIdList, toolbox } =
+    useCanvasStore(
+      useShallow((store) => ({
+        selectedElementIdList: store.selectedElementIdList,
+        deleteElement: store.deleteElement,
+        isFocused: store.isFocused,
+        toolbox: store.toolbox,
+      })),
+    );
 
   useEffect(() => {
     const handleDeleteElement = (e: KeyboardEvent) => {
-      if (e.key === 'Delete' && isFocused && selectedElementIdList.length > 0) {
-        deleteElement(...selectedElementIdList);
+      if (toolbox.action === 'SELECT') {
+        if (e.key === 'Delete' && isFocused) {
+          if (selectedElementIdList.length > 0) {
+            deleteElement(...selectedElementIdList);
+          }
+        }
       }
     };
 
     document.addEventListener('keydown', handleDeleteElement, false);
     return () => document.removeEventListener('keydown', handleDeleteElement);
-  }, [selectedElementIdList, isFocused]);
+  }, [selectedElementIdList, isFocused, toolbox.action]);
 }
